7c7cc256fad1a28b183f60378075578b2c2061a1
Tony Chia <tchia@spokeo.com>
2019-08-08 13:07:53 -0700

    Sync up the version

--- component.sh
+++ component.sh
@@ -31,8 +31,12 @@ function upload_gem_pkg {
   RESULT=$(grep "received and indexed" upload_output.log)
   if [ "$RESULT" != "" ]; then
     echo "Uplaod Successful. You should see the new verson on https://gems.spokeo.com"
+    echo "Current browserstack-automate-api packages on https://gemspokeo.com"
+    gem search -ra ^browserstack-automate-api$ --source https://gems.spokeo.com
   else
     echo "ERROR: Uplaod failed."
+    echo "Current browserstack-automate-api packages on https://gemspokeo.com"
+    gem search -ra ^browserstack-automate-api$ --source https://gems.spokeo.com
     exit 1
   fi
 }
@@ -41,7 +45,8 @@ function push_version_change_back_to_github {
   echo "Finding the changed files. Should be only the ./lib/browserstack/automate/api/version.rb"
   git status
   echo "Adding changed file to be commited"
-  git add -u
+  git add -u lib/browserstack/automate/api/version.rb
+  git status
   echo "commit the change"
   git commit -m "Updated from $BUILD_URL"
   echo "Push to remote git repo"
--- lib/browserstack/automate/api/version.rb
+++ lib/browserstack/automate/api/version.rb
@@ -3,7 +3,7 @@
 module Browserstack
   module Automate
     module Api
-      VERSION = '0.1.4'
+      VERSION = '0.1.5'
     end
   end
 end
